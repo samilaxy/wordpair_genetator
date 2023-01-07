f8e2f229570b6768659fd4697dcd0ef91f853997
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-01-07 21:01:09 +0000

    Update random_words.dart

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -38,17 +38,32 @@ class RandomWordsState extends State<RandomWords> {
           });
         });
   }
-void _pushSaved(){
 
-}
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = savedWordPairs.map((WordPair pair) {
+        return ListTile(
+          title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)),
+        );
+      });
+      final List<Widget> divided =
+          ListTile.divideTiles(
+            context: context, 
+            tiles: tiles
+            ).toList();
+      return Scaffold(
+          appBar: AppBar(
+        title: Text('Saved WordPairs')),
+         body: ListView(children: divided)
+         );
+    }));
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('WordPair Generator'),
-      actions: <Widget>[
-        IconButton(
-          onPressed: _pushSaved,
-           icon: Icon(Icons.list))
+      appBar: AppBar(title: Text('WordPair Generator'), actions: <Widget>[
+        IconButton(onPressed: _pushSaved, icon: Icon(Icons.list))
       ]),
       body: _buildList(),
     );
